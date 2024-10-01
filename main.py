53081adaeec18bfd9fd3e487e1350647142860fa
TrLSeysh <trlseysh@gmail.com>
2024-10-01 14:49:53 +0200

    add python functions & main

---
+++ main.py
@@ -0,0 +1,20 @@
+from code import DecisionTree
+from code import LassoRegressor
+from code import RandomForest
+from code import RidgeRegressor
+from code import SVM
+
+
+def main():
+    """
+
+    Mettez votre l'appel de chaque fonction ici.
+    Exemple :
+
+    DecisionTree.main(data)
+    """
+    print("Hello")
+
+
+if __name__ == '__main__':
+    main()
