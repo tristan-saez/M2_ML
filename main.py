1aae651e2f3877abca31e7832fb8512c9d0b20cd
TrLSeysh <trlseysh@gmail.com>
2024-10-01 15:34:40 +0200

    modify call functions

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pandas as pd
-import algorithms as al
+from algorithms import DecisionTree
+from algorithms import RandomForest
+from algorithms import RidgeRegressor
+from algorithms import LassoRegressor
+from algorithms import SVM_karim
+from algorithms import SVM_tristan
 
 
 def main():
@@ -14,24 +19,28 @@ def main():
     ozone_bdd = pd.read_table("data/ozone_complet.txt", sep = ";")
 
     print("=" * 50, "\nCHOIX ALGORITHME\n"+"=" * 50+"\n1. Arbre de décisions\n2. Forêts aléatoires\n"
-                                                    "3. Régression ridge\n4. Régression lasso\n5. SVM")
+                                                    "3. Régression ridge\n4. Régression lasso\n"
+                                                    "5. SVM (karim)\n6. SVM (tristan)")
     choice = int(input("\n>"))
 
     if choice == 1:
         print("=" * 50, "\nARBRE DE DÉCISION\n" + "=" * 50)
-        al.DecisionTree.decision_tree(car_seats_bdd)
+        DecisionTree.decision_tree(car_seats_bdd)
     elif choice == 2:
         print("=" * 50, "\nFORÊTS ALÉATOIRES\n" + "=" * 50)
-        al.RandomForest.random_forest(car_seats_bdd)
+        RandomForest.random_forest(car_seats_bdd)
     elif choice == 3:
         print("=" * 50, "\nRÉGRESSION RIDGE\n" + "=" * 50)
-        al.RidgeRegressor.ridge_regressor(ozone_bdd)
+        RidgeRegressor.ridge_regressor(ozone_bdd)
     elif choice == 4:
         print("=" * 50, "\nRÉGRESSION LASSO\n" + "=" * 50)
-        al.LassoRegressor.lasso_regressor(ozone_bdd)
+        LassoRegressor.lasso_regressor(ozone_bdd)
     elif choice == 5:
         print("=" * 50, "\nSVM\n" + "=" * 50)
-        al.SVM.svm(ozone_bdd)
+        SVM_karim.svm(ozone_bdd)
+    elif choice == 6:
+        print("=" * 50, "\nSVM\n" + "=" * 50)
+        SVM_tristan.svm(ozone_bdd)
     else:
         print("Choix non reconnu")
 
