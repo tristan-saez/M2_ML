8a8c46d9e14d027945a048e1fa4c8a241ebfd502
TrLSeysh <trlseysh@gmail.com>
2024-10-01 15:18:15 +0200

    modify main

--- main.py
+++ main.py
@@ -15,6 +15,22 @@ def main():
 
     print("=" * 50, "\nCHOIX ALGORITHME\n"+"=" * 50+"\n1. Arbre de décisions\n2. Forêts aléatoires\n"
                                                     "3. Régression ridge\n4. Régression lasso\n5. SVM")
-    choice = input("\n>")
+    choice = int(input("\n>"))
+
+    if choice == 1:
+        print("=" * 50, "\nARBRE DE DÉCISION\n" + "=" * 50)
+    elif choice == 2:
+        print("=" * 50, "\nFORÊTS ALÉATOIRES\n" + "=" * 50)
+    elif choice == 3:
+        print("=" * 50, "\nRÉGRESSION RIDGE\n" + "=" * 50)
+    elif choice == 4:
+        print("=" * 50, "\nRÉGRESSION LASSO\n" + "=" * 50)
+    elif choice == 5:
+        print("=" * 50, "\nSVM\n" + "=" * 50)
+    else:
+        print("Choix non reconnu")
+
+
+
 if __name__ == '__main__':
     main()
