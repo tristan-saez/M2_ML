9c2ca3d13bbf7c4d0118150ca8ae9ce2e5d057e5
creismeas Maël <mael.creismeas@outlook.fr>
2024-10-03 16:34:47 +0200

    update

--- algorithms/RidgeRegressor.py
+++ algorithms/RidgeRegressor.py
@@ -1,48 +1,108 @@
-def ridge_regressor(data):
+# Importing libraries 
+  
+import numpy as np 
+import pandas as pd 
+
+  
+# Ridge Regression 
+  
+class RidgeRegressor() : 
+      
+    def __init__( self, learning_rate, iterations, l2_penality ) : 
+          
+        self.learning_rate = learning_rate         
+        self.iterations = iterations         
+        self.l2_penality = l2_penality 
+          
+    # Function for model training             
+    def fit( self, X, Y ) : 
+          
+        # no_of_training_examples, no_of_features         
+        self.m, self.n = X.shape 
+          
+        # weight initialization         
+        self.W = np.zeros( self.n ) 
+          
+        self.b = 0        
+        self.X = X         
+        self.Y = Y 
+          
+        # gradient descent learning 
+                  
+        for i in range( self.iterations ) :             
+            self.update_weights()             
+        return self
+      
+    # Helper function to update weights in gradient descent 
+      
+    def update_weights( self ) :            
+        Y_pred = self.predict( self.X ) 
+          
+        # calculate gradients       
+        dW = ( - ( 2 * ( self.X.T ).dot( self.Y - Y_pred ) ) +               
+               ( 2 * self.l2_penality * self.W ) ) / self.m      
+        db = - 2 * np.sum( self.Y - Y_pred ) / self.m  
+          
+        # update weights     
+        self.W = self.W - self.learning_rate * dW     
+        self.b = self.b - self.learning_rate * db         
+        return self
+      
+    # Hypothetical function  h( x )  
+    def predict( self, X ) :     
+        return X.dot( self.W ) + self.b 
+    
+    
+"""
+def ridge_regressor(X_train, X_test, Y_train, Y_test ):
     import numpy as np
     import pandas as pd
     from numpy.linalg import inv
+    
 
     LAMBDA=2
     # Define dataset (X,y)
-    X0 = np.array([[0.8,  1.2,  0.5,  -0.7, 1.0],
-              [1.0,  0.8,  -0.4, 0.5,  -1.2],
-              [-0.5, 0.3,  1.2,  0.9,  -0.1],
-              [0.2,  -0.9, -0.7, 1.1,  0.5]])
+    #X0 = np.array([[0.8,  1.2,  0.5,  -0.7, 1.0],
+    #          [1.0,  0.8,  -0.4, 0.5,  -1.2],
+    #          [-0.5, 0.3,  1.2,  0.9,  -0.1],
+    #          [0.2,  -0.9, -0.7, 1.1,  0.5]])
 
-    y0 = np.array([3.2, 2.5, 1.8, 2.9])
+    #y0 = np.array([3.2, 2.5, 1.8, 2.9])
+
+    X=X_train.to_numpy()
+    Y=Y_test.to_numpy()
 
     #X[["T6","T9","T12","T15","T18","Ne6","Ne9","Ne12","Ne15","Ne18","Vdir6","Vvit6","Vdir9","Vvit9","Vdir12","Vvit12","Vdir15","Vvit15","Vdir18","Vvit18","Vx"]]=data.to_numpy()
-    X=data.to_numpy()
-    y=data.index.to_numpy()
-    test1=X.nanmean(axis=0)
-    test2=X.nanstd(axis=0)
-    test3=X0.mean(axis=0)
-    test4=X0.std(axis=0)
+    #X=data.to_numpy()
+    #y=data.index.to_numpy()
+    #test1=X.nanmean(axis=0)
+    #test2=X.nanstd(axis=0)
+    #test3=X0.mean(axis=0)
+    #test4=X0.std(axis=0)
     # Scale predictors
-    X_scale = (X-X.nanmean(axis=0))/X.nanstd(axis=0)
+    X_scale = (X-X.mean(axis=0))/X.std(axis=0)
 
     # RIDGE REGRESSION MODEL - coefficients estimation
     # X*X^T + LAMBDA*I
-    x1 = np.matmul(X_scale.T, X_scale) + LAMBDA*np.identity(5)
+    x1 = np.matmul(X_scale.T, X_scale) + LAMBDA*np.identity(21)
     # Transpose obtained matrix - (X*X^T + LAMBDA*I)^{-1}
     x1_inv = inv(x1)
     # ( (X*X^T + LAMBDA*I)^{-1} ) * X^T
     x2 = np.matmul(x1_inv, X_scale.T)
     # ( ( (X*X^T + LAMBDA*I)^{-1} ) * X^T ) * Y
-    coef = np.matmul(x2, y)
+    coef = np.matmul(x2, Y)
     # Estimated coeficients
     print(coef)
 
     # predictions
-    np.matmul(X_scale, coef)+y.mean()
+    np.matmul(X_scale, coef)+Y.mean()
 
 
     #pass
 
 #https://stackoverflow.com/questions/13187778/convert-pandas-dataframe-to-numpy-array
 #faire une fonction stnaderdised(...) Qui standardise les donnée en soustrayant par la moyenne et divisant par l'écar type
-
+"""
 """
 import numpy as np
 import pandas as pd
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pandas as pd
-
+import numpy as np
+import os
+import matplotlib.pyplot as plt 
+from algorithms import NormalizeOzone
 from algorithms import RidgeRegressor
 
 
@@ -13,11 +16,58 @@ def main():
     DecisionTree.main(data)
     """
     #car_seats_bdd = pd.read_csv("data/Carseats.csv")
-    ozone_bdd = pd.read_table("data/ozone_complet.txt", sep = ";")
-    print(ozone_bdd)
-    RidgeRegressor.ridge_regressor(ozone_bdd)
-    print("fin")
+    #ozone_bdd = pd.read_table("data/ozone_complet.txt", sep = ";")
+    #print(ozone_bdd)
+    cwd=os.getcwd()
+    X_train, X_test, Y_train, Y_test = NormalizeOzone.PreProcessingOzone(cwd+'/data/ozone_complet.txt', ';')
+    #RidgeRegressor.ridge_regressor(X_train, X_test, Y_train, Y_test )
+    # Driver code 
+  
+    # Model training     
+    model = RidgeRegressor.RidgeRegressor( iterations = 1000,learning_rate = 0.01, l2_penality = 0.1 ) 
+    model.fit( X_train, Y_train ) 
+      
+    # Prediction on test set 
+    Y_pred = model.predict( X_test )     
+    print( "Predicted values ", np.round( Y_pred[:3], 2 ) )      
+    print( "Real values      ", Y_test[:3] )     
+    print( "Trained W        ", round( model.W[0], 2 ) )     
+    print( "Trained b        ", round( model.b, 2 ) ) 
 
+    i=0
+    mean_absolute_error=0
+    for pred in Y_pred:
+        mean_absolute_error+=abs(pred-Y_test[i])
+        i+=1
+    mean_absolute_error=mean_absolute_error/i
+    print("mean absoulte error : ", mean_absolute_error)
+    """
+    print("1")
+    print(Y_test)
+    print("2")
+    print(Y_pred)
+    print("3")
+    print(Y_test.shape)
+    print("4")
+    print(Y_pred.shape)
+    print("5")
+    print(Y_test.index)
+    print("6")
+    print(Y_pred.index)
+    print("ok")
+
+    # Visualization on test set  
+    Y_test_plot=Y_test.to_numpy()   
+    Y_pred_plot=Y_pred.to_numpy()    
+    Y_test_plot_index=Y_test.index.to_numpy()    
+    Y_pred_plot_index=Y_pred.index.to_numpy()     
+    print(Y_pred_plot.shape)
+    print(Y_test_plot.shape)
+    print(Y_test_plot_index.shape)
+    plt.scatter( Y_test_plot_index, Y_test_plot, color = 'blue' )     
+    plt.plot( Y_test_plot_index, Y_pred_plot, color = 'orange' )
+    plt.show() 
+    """
 
 if __name__ == '__main__':
     main()
