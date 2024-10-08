e2268e40f1d87bd4773ca42a61cf6dd81427a712
creismeas Maël <mael.creismeas@outlook.fr>
2024-10-08 15:43:57 +0200

    Update RidgeRegressor.py

--- algorithms/RidgeRegressor.py
+++ algorithms/RidgeRegressor.py
@@ -11,41 +11,49 @@ from sklearn.linear_model import Ridge
 class RidgeRegressor() : 
       
     def __init__( self, learning_rate, iterations, l2_penality ) : 
-          
+        #On initialise les parametres renseignés
         self.learning_rate = learning_rate         
         self.iterations = iterations         
         self.l2_penality = l2_penality 
           
-    # Function for model training             
+               
     def fit( self, X, Y ) : 
           
-        # no_of_training_examples, no_of_features         
+        # no_of_training_examples, no_of_features  
+        # self.m correspon au nombre de parametres (utiliser pour calcul du gradient)   
+        # self.n correspond au nombre de features (utiliser pour taille de la matrice des poids)    
         self.m, self.n = X.shape 
           
-        # weight initialization         
+        #On initialise la matrice des poids à zéro       
         self.W = np.zeros( self.n ) 
           
         self.b = 0        
         self.X = X         
         self.Y = Y 
           
-        # gradient descent learning 
-                  
+        #multiples itérations (correspondant au nombre d'époch) d'aprentissage et d'adaptation des points pour
+        #minimiser l'écart entre résultat trouvé et résultat recherché.
         for i in range( self.iterations ) :             
             self.update_weights()             
         return self
       
-    # Helper function to update weights in gradient descent 
-      
-    def update_weights( self ) :            
+    #Fonction d'aprentissage 
+    def update_weights( self ) : 
+        #On prédit une premiere fois les valeurs pour comparer le résultat obtenu avec celui attendu           
         Y_pred = self.predict( self.X ) 
           
-        # calculate gradients       
+        #La méthode d'aprentissage marche par descente de gradient. On doit donc calculer les gradients qui viendront
+        # mettre à jours nos poids qui permetent de prédire les valeurs.
+        #Pour obtenir ces gradients on utilise la formule si dessous qui compare les résultats obtenus aux résultats
+        # attendu et divise par le nombre de parametres 
         dW = ( - ( 2 * ( self.X.T ).dot( self.Y - Y_pred ) ) +               
                ( 2 * self.l2_penality * self.W ) ) / self.m      
         db = - 2 * np.sum( self.Y - Y_pred ) / self.m  
           
-        # update weights     
+        #A partir des gradients calculer juste avant, on vient metre a jours les poids qui servent à prédires les valeur
+        #Les poids sont mis à jours dans le but qu'ils puissent prédire avec plus de précision par la suite
+        #De plus, la modification des poids est limiter par le learning rate. Cela fait que le réseau n'est pas 
+        #Dépendant d'une seul epochs mais demande ducoup plusieurs epochs d'aprentissage.
         self.W = self.W - self.learning_rate * dW     
         self.b = self.b - self.learning_rate * db         
         return self
