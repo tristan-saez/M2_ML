54d4b8f02582261fbf4bfb58cd02e936f41d3833
TrLSeysh <tristan.saez3@gmail.com>
2024-10-08 17:36:29 +0200

    update main.py

--- main.py
+++ main.py
@@ -20,35 +20,56 @@ def main():
 
     DecisionTree.main(data)
     """
+
+    # Effectue les étapes de pre-processing
     x_train_carseats, x_test_carseats, y_train_carseats, y_test_carseats = (
         NormalizeCarseats.PreProcessingCarSeats("data/Carseats.csv", ","))
 
+    # Effectue les étapes de pre-processing
     x_train_ozone, x_test_ozone, y_train_ozone, y_test_ozone = (
         NormalizeOzone.PreProcessingOzone("data/ozone_complet.txt", ";"))
 
+    # Permet à l'utilisateur de choisir l'algorithme à entraîner
     print("=" * 50, "\nCHOIX ALGORITHME\n"+"=" * 50+"\n1. Arbre de décisions\n2. Forêts aléatoires\n"
                                                     "3. Régression ridge\n4. Régression lasso\n"
                                                     "5. SVM (Classification)\n6. SVM (Regression)")
 
     choice = int(input("\n>"))
 
-    start_time = timer()
-
     if choice == 1:
-        start_time = timer()
         print("=" * 50, "\nARBRE DE DÉCISION\n" + "=" * 50)
-        y_test, y_pred = DecisionTree.decision_tree(x_train_carseats, x_test_carseats, y_train_carseats, y_test_carseats)
+        if model_choice() == 1:
+            # Lance le chronomètre de temps d'apprentissage
+            start_time = timer()
+            y_test, y_pred = DecisionTree.decision_tree(x_train_carseats, x_test_carseats, y_train_carseats, y_test_carseats)
+        else:
+            # Lance le chronomètre de temps d'apprentissage
+            start_time = timer()
+            y_test, y_pred = DecisionTree.decision_tree_sklearn(x_train_carseats, x_test_carseats, y_train_carseats, y_test_carseats)
+
+        # Arrête le chronomètre de temps d'apprentissage
         end_time = timer()
+        # Vérifie les différents scores en fonction du modèle de Machine Learning (regression/classification)
         CheckScore.check_score("classifier", y_test, y_pred, start_time, end_time)
         
     elif choice == 2:
-        start_time = timer()
         print("=" * 50, "\nFORÊTS ALÉATOIRES\n" + "=" * 50)
-        y_test, y_pred = RandomForest.random_forest(x_train_carseats, x_test_carseats, y_train_carseats, y_test_carseats)
+        if model_choice() == 1:
+            # Lance le chronomètre de temps d'apprentissage
+            start_time = timer()
+            y_test, y_pred = RandomForest.random_forest(x_train_carseats, x_test_carseats, y_train_carseats, y_test_carseats)
+        else:
+            # Lance le chronomètre de temps d'apprentissage
+            start_time = timer()
+            y_test, y_pred = RandomForest.random_forest(x_train_carseats, x_test_carseats, y_train_carseats, y_test_carseats)
+
+        # Arrête le chronomètre de temps d'apprentissage
         end_time = timer()
+        # Vérifie les différents scores en fonction du modèle de Machine Learning (regression/classification)
         CheckScore.check_score("classifier", y_test, y_pred, start_time, end_time)
         
     elif choice == 3:
+        # Lance le chronomètre de temps d'apprentissage
         start_time1 = timer()
         #On appelle la classe RidgeRegressor pour créer le modèle. Ce dernier est paramétré pour apprendre sur 1000 epochs avec 
         #un taux d'apprentissage de 0.01. De plus, le coefficient de pénalité associer à l'algorithme de Ridge est de 5
@@ -61,7 +82,7 @@ def main():
         end_time1 = timer()
         CheckScore.check_score("regressor", y_test, y_pred, start_time1, end_time1)
 
-
+        # Lance le chronomètre de temps d'apprentissage
         start_time2 = timer()
         #On appel la classe RidgeRegressorSklearn, qui vient créer le modèle. Ce modèle utilise les méthodes issues de sklearn
         #De plus, le coefficient de pénalité associer à l'algorithme de Ridge est toujours de 5 pour comparer au modèle précédent.
@@ -78,42 +99,65 @@ def main():
     elif choice == 4:
         print("=" * 50, "\nRÉGRESSION LASSO\n" + "=" * 50)
         if model_choice() == 1:
-          start_time = timer()
-          y_test, y_pred = LassoRegressor.lasso_regressor(x_train_ozone, x_test_ozone, y_train_ozone, y_test_ozone)
+            # Lance le chronomètre de temps d'apprentissage
+            start_time = timer()
+            y_test, y_pred = LassoRegressor.lasso_regressor(x_train_ozone, x_test_ozone, y_train_ozone, y_test_ozone)
         else:
-          start_time = timer()
-          y_test, y_pred = LassoRegressor.lasso_regressor_sklearn(x_train_ozone, x_test_ozone, y_train_ozone, y_test_ozone)
+            # Lance le chronomètre de temps d'apprentissage
+            start_time = timer()
+            y_test, y_pred = LassoRegressor.lasso_regressor_sklearn(x_train_ozone, x_test_ozone, y_train_ozone, y_test_ozone)
+
+        # Arrête le chronomètre de temps d'apprentissage
         end_time = timer()
+        # Vérifie les différents scores en fonction du modèle de Machine Learning (regression/classification)
         CheckScore.check_score("regressor", y_test, y_pred, start_time, end_time)
 
     elif choice == 5:
         print("=" * 50, "\nSVM\n" + "=" * 50)
         if model_choice() == 1:
-          start_time = timer()
-          y_test, y_pred = SVM_Carseats.svm(x_train_carseats, x_test_carseats, y_train_carseats, y_test_carseats)
+            # Lance le chronomètre de temps d'apprentissage
+            start_time = timer()
+            y_test, y_pred = SVM_Carseats.svm(x_train_carseats, x_test_carseats, y_train_carseats, y_test_carseats)
         else:
+            # Lance le chronomètre de temps d'apprentissage
             start_time = timer()
             y_test, y_pred = sklearn_svm.svm_classification(x_train_carseats, x_test_carseats, y_train_carseats, y_test_carseats)
+
+        # Arrête le chronomètre de temps d'apprentissage
         end_time = timer()
+        # Vérifie les différents scores en fonction du modèle de Machine Learning (regression/classification)
         CheckScore.check_score("classifier", y_test, y_pred, start_time, end_time)
 
     elif choice == 6:
         print("=" * 50, "\nSVM\n" + "=" * 50)
         if model_choice() == 1:
-          start_time = timer()
-          y_test, y_pred = SVM_Ozone.svm(x_train_ozone, x_test_ozone, y_train_ozone, y_test_ozone)
+            # Lance le chronomètre de temps d'apprentissage
+            start_time = timer()
+            y_test, y_pred = SVM_Ozone.svm(x_train_ozone, x_test_ozone, y_train_ozone, y_test_ozone)
         else:
-          start_time = timer()
-          y_test, y_pred = sklearn_svm.svm_reg(x_train_ozone, x_test_ozone, y_train_ozone, y_test_ozone)
+            # Lance le chronomètre de temps d'apprentissage
+            start_time = timer()
+            y_test, y_pred = sklearn_svm.svm_reg(x_train_ozone, x_test_ozone, y_train_ozone, y_test_ozone)
+
+        # Arrête le chronomètre de temps d'apprentissage
         end_time = timer()
+        # Vérifie les différents scores en fonction du modèle de Machine Learning (regression/classification)
         CheckScore.check_score("regressor", y_test, y_pred, start_time, end_time)
 
     else:
         print("Choix non reconnu")
 
+
 def model_choice():
+    """
+
+    Demande à l'utilisateur de choisir entre le modèle "from scratch" et le modèle scikit-learn
+
+    :return: modèle choisi (en entier : 1 ou 2)
+    """
     print("\nSelectionnez le choix d'algortihme\n1. From \"scratch\"\n2. Scikit-learn\n")
     return int(input(">"))
+
 
 if __name__ == '__main__':
     main()
