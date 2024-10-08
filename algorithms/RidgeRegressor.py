6f89634ca04af5fe9e34df7b3bd0dee7cfe86aca
creismeas Maël <mael.creismeas@outlook.fr>
2024-10-08 16:24:38 +0200

    update

--- algorithms/RidgeRegressor.py
+++ algorithms/RidgeRegressor.py
@@ -6,12 +6,10 @@ import pandas as pd
 from sklearn.linear_model import Ridge
 
   
-# Ridge Regression 
-  
 class RidgeRegressor() : 
       
     def __init__( self, learning_rate, iterations, l2_penality ) : 
-        #On initialise les parametres renseignés
+        #On initialise les paramètres renseignés
         self.learning_rate = learning_rate         
         self.iterations = iterations         
         self.l2_penality = l2_penality 
@@ -19,9 +17,8 @@ class RidgeRegressor() :
                
     def fit( self, X, Y ) : 
           
-        # no_of_training_examples, no_of_features  
-        # self.m correspon au nombre de parametres (utiliser pour calcul du gradient)   
-        # self.n correspond au nombre de features (utiliser pour taille de la matrice des poids)    
+        # self.m correspond au nombre de paramètres et sera utilisé pour calcul du gradient (division par nombre de paramètres
+        # self.n correspond au nombre de features et sera utilisé pour taille de la matrice des poids     
         self.m, self.n = X.shape 
           
         #On initialise la matrice des poids à zéro       
@@ -31,42 +28,42 @@ class RidgeRegressor() :
         self.X = X         
         self.Y = Y 
           
-        #multiples itérations (correspondant au nombre d'époch) d'aprentissage et d'adaptation des points pour
+        #Multiples itérations (correspondant au nombre d'epochs) d'apprentissage et d'adaptation des points pour
         #minimiser l'écart entre résultat trouvé et résultat recherché.
         for i in range( self.iterations ) :             
             self.update_weights()             
         return self
       
-    #Fonction d'aprentissage 
+    #Fonction d'apprentissage 
     def update_weights( self ) : 
-        #On prédit une premiere fois les valeurs pour comparer le résultat obtenu avec celui attendu           
+        #On prédit pour cette epoch les valeurs pour comparer le résultat obtenu avec celui attendu           
         Y_pred = self.predict( self.X ) 
           
-        #La méthode d'aprentissage marche par descente de gradient. On doit donc calculer les gradients qui viendront
-        # mettre à jours nos poids qui permetent de prédire les valeurs.
+        #La méthode d'apprentissage marche par descente de gradient. On doit donc calculer les gradients qui viendront
+        # mettre à jours nos poids qui permettent de prédire les valeurs.
         #Pour obtenir ces gradients on utilise la formule si dessous qui compare les résultats obtenus aux résultats
-        # attendu et divise par le nombre de parametres 
+        # attendu et divise par le nombre de paramètres 
         dW = ( - ( 2 * ( self.X.T ).dot( self.Y - Y_pred ) ) +               
                ( 2 * self.l2_penality * self.W ) ) / self.m      
         db = - 2 * np.sum( self.Y - Y_pred ) / self.m  
           
-        #A partir des gradients calculer juste avant, on vient metre a jours les poids qui servent à prédires les valeur
-        #Les poids sont mis à jours dans le but qu'ils puissent prédire avec plus de précision par la suite
-        #De plus, la modification des poids est limiter par le learning rate. Cela fait que le réseau n'est pas 
-        #Dépendant d'une seul epochs mais demande ducoup plusieurs epochs d'aprentissage.
+        #A partir des gradients calculer juste avant, on vient mettre à jours les poids qui servent à prédire les valeurs
+        #Les poids sont mis à jour dans le but qu'ils puissent prédire avec plus de précision par la suite
+        #De plus, la modification des poids est limitée par le learning rate. Cela fait que les poids du modéle ne 
+        #Sont pas dépendant d'une seul epoch mais demande ducoup plusieurs epochs d'apprentissage.
         self.W = self.W - self.learning_rate * dW     
         self.b = self.b - self.learning_rate * db         
         return self
       
-    #On prédit les valeurs de sorties en fonction des valeurs d'entrée et de la matrice de poid établie
-    #et entrainné plus tot
+    #On prédit les valeurs de sorties en fonction des valeurs d'entrée et de la matrice de poids établie
+    #et entraîné plus tôt
     def predict( self, X ) :     
         return X.dot( self.W ) + self.b 
     
 
 
 class RidgeRegressorSklearn():
-    #Mise au point d'un modéle de ridge régression en utilisant les fonction sklearn
+    #Mise au point d'un modèle de ridge régression en utilisant les fonctions sklearn
     def __init__( self , l2_penality) : 
         self.l2_penality = l2_penality 
         self.modele_ridge_regressor = Ridge(alpha=self.l2_penality )
--- main.py
+++ main.py
@@ -17,22 +17,22 @@ def main():
 
     DecisionTree.main(data)
     """
-    #On obtient les données à répartir en base de test et d'aprentissage
+    #On obtient les données à répartir en base de test et d'apprentissage
     cwd=os.getcwd()
     X_train, X_test, Y_train, Y_test = NormalizeOzone.PreProcessingOzone(cwd+'/data/ozone_complet.txt', ';')
-    #La fonction PreProcessingOzone() renvoie répartie 70% des données dans la base d'aprentissage et 30% 
+    #La fonction PreProcessingOzone() renvoie répartie 70% des données dans la base d'apprentissage et 30% 
     #dans la base de test
 
-    #mise en place d'un timer pour calcul du temps d'execution
+    #Mise en place d'un timer pour calcul du temps d'exécution
     start_time1 = time.time()
   
-    #On appel la classe RidgeRegressor pour créer le modéle. Ce dernier est parametrer pour aprendre sur 1000 epochs avec 
-    #un taut d'aprentissage de 0.01. De plus, le coefficient de pénalité associer à l'algorythme de Ridge est de 5
+    #On appelle la classe RidgeRegressor pour créer le modèle. Ce dernier est paramétré pour apprendre sur 1000 epochs avec 
+    #un taux d'apprentissage de 0.01. De plus, le coefficient de pénalité associer à l'algorithme de Ridge est de 5
     model = RidgeRegressor.RidgeRegressor( iterations = 1000,learning_rate = 0.01, l2_penality = 5 ) 
-    #On entraine le modéles avec les précédents paramatre et la base d'entrainement
+    #On entraine le modèle avec les précédents paramètres et la base d'entrainement
     model.fit(X_train, Y_train) 
       
-    #On prédit les valeurs de notre base de test à partir du modéle entrainné
+    #On prédit les valeurs de notre base de test à partir du modèle entraîné
     Y_pred = model.predict( X_test )     
     
     i=0
@@ -48,19 +48,19 @@ def main():
 
 
 ###############################################################################################
-#A PARTIR DE MAINTENANT METHODE AVEC sklearn
+#A PARTIR DE MAINTENANT METHODE AVEC SKLEARN
 ###############################################################################################
 
-    #mise en place d'un timer pour calcul du temps d'execution
+    #Mise en place d'un timer pour calcul du temps d'exécution
     start_time2 = time.time()
   
-    #On appel la classse RidgeRegressorSklearn, qui vient créer le modéle. Ce modéle utilise les methodes issus de sklearn
-    #De plus, le coefficient de pénalité associer à l'algorythme de Ridge est toujours de 5 pour comparer au modéle précédent.
+    #On appel la classe RidgeRegressorSklearn, qui vient créer le modèle. Ce modèle utilise les méthodes issues de sklearn
+    #De plus, le coefficient de pénalité associer à l'algorithme de Ridge est toujours de 5 pour comparer au modèle précédent.
     model2 = RidgeRegressor.RidgeRegressorSklearn(l2_penality = 5) 
-    #On entraine le modéles avec les précédents paramatre et la base d'entrainement
+    #On entraine le modèle avec les paramètres précédents et la base d'entrainement
     model2.fit(X_train, Y_train) 
       
-    #On prédit les valeurs de notre base de test à partir du modéle entrainné
+    #On prédit les valeurs de notre base de test à partir du modèle entraîné
     Y_pred2 = model2.predict( X_test )     
 
     i=0
@@ -71,7 +71,7 @@ def main():
         i+=1
         y+=1
     mean_absolute_error2=mean_absolute_error2/i
-    
+
     print("--- %s secondes pour le ridge regressor avec sklearn ---" % (time.time() - start_time2))
     print("mean absoulte error de ridge regressor avec sklearn : ", mean_absolute_error2)
     
