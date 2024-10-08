16b353db0e6ebf91a02c07fc5843ca2b5e58be0c
creismeas Maël <mael.creismeas@outlook.fr>
2024-10-08 15:20:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import os
 import matplotlib.pyplot as plt 
 from algorithms import NormalizeOzone
 from algorithms import RidgeRegressor
-from algorithms import RidgeRegressorSklearn
 import time
 
 
@@ -18,21 +17,22 @@ def main():
 
     DecisionTree.main(data)
     """
-    #car_seats_bdd = pd.read_csv("data/Carseats.csv")
-    #ozone_bdd = pd.read_table("data/ozone_complet.txt", sep = ";")
-    #print(ozone_bdd)
+    #On obtient les données à répartir en base de test et d'aprentissage
     cwd=os.getcwd()
     X_train, X_test, Y_train, Y_test = NormalizeOzone.PreProcessingOzone(cwd+'/data/ozone_complet.txt', ';')
-    #RidgeRegressor.ridge_regressor(X_train, X_test, Y_train, Y_test )
-    # Driver code 
+    #La fonction PreProcessingOzone() renvoie répartie 70% des données dans la base d'aprentissage et 30% 
+    #dans la base de test
 
+    #mise en place d'un timer pour calcul du temps d'execution
     start_time1 = time.time()
   
-    # Model training     
+    #On appel la classe RidgeRegressor pour créer le modéle. Ce dernier est parametrer pour aprendre sur 1000 epochs avec 
+    #un taut d'aprentissage de 0.01. De plus, le coefficient de pénalité associer à l'algorythme de Ridge est de 5
     model = RidgeRegressor.RidgeRegressor( iterations = 1000,learning_rate = 0.01, l2_penality = 5 ) 
+    #On entraine le modéles avec les précédents paramatre et la base d'entrainement
     model.fit(X_train, Y_train) 
       
-    # Prediction on test set 
+    #On prédit les valeurs de notre base de test à partir du modéle entrainné
     Y_pred = model.predict( X_test )     
     
     i=0
@@ -41,6 +41,7 @@ def main():
         mean_absolute_error+=abs(Y_pred[index]-Y_test[index])
         i+=1
     mean_absolute_error=mean_absolute_error/i
+
     print("--- %s secondes pour le ridge regressor sans sklearn ---" % (time.time() - start_time1))
     print("mean absoulte error de ridge regressor sans sklearn : ", mean_absolute_error)
 
@@ -50,24 +51,29 @@ def main():
 #A PARTIR DE MAINTENANT METHODE AVEC sklearn
 ###############################################################################################
 
-
+    #mise en place d'un timer pour calcul du temps d'execution
     start_time2 = time.time()
   
-    # Model training     
-    model2 = RidgeRegressorSklearn.RidgeRegressorSklearn( iterations = 1000,learning_rate = 0.01, l2_penality = 5 ) 
-    model.fit(X_train, Y_train) 
+    #On appel la classse RidgeRegressorSklearn, qui vient créer le modéle. Ce modéle utilise les methodes issus de sklearn
+    #De plus, le coefficient de pénalité associer à l'algorythme de Ridge est toujours de 5 pour comparer au modéle précédent.
+    model2 = RidgeRegressor.RidgeRegressorSklearn(l2_penality = 5) 
+    #On entraine le modéles avec les précédents paramatre et la base d'entrainement
+    model2.fit(X_train, Y_train) 
       
-    # Prediction on test set 
-    Y_pred = model.predict( X_test )     
+    #On prédit les valeurs de notre base de test à partir du modéle entrainné
+    Y_pred2 = model2.predict( X_test )     
 
     i=0
-    mean_absolute_error=0
-    for index in Y_pred.index:
-        mean_absolute_error+=abs(Y_pred[index]-Y_test[index])
+    y=0
+    mean_absolute_error2=0
+    for index in Y_test.index:
+        mean_absolute_error2+=abs(Y_pred2[y]-Y_test[index])
         i+=1
-    mean_absolute_error=mean_absolute_error/i
-    print("--- %s secondes pour le ridge regressor sans sklearn ---" % (time.time() - start_time2))
-    print("mean absoulte error de ridge regressor sans sklearn : ", mean_absolute_error)
+        y+=1
+    mean_absolute_error2=mean_absolute_error2/i
+    
+    print("--- %s secondes pour le ridge regressor avec sklearn ---" % (time.time() - start_time2))
+    print("mean absoulte error de ridge regressor avec sklearn : ", mean_absolute_error2)
     
 
 if __name__ == '__main__':
